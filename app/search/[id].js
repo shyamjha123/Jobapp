c27bfa38c1c47514b83f1c7f469617d12fa7c450
shyamjha123 <109013031+shyamjha123@users.noreply.github.com>
2024-01-03 16:28:28 +0530

    Update [id].js

--- app/search/[id].js
+++ app/search/[id].js
@@ -26,7 +26,7 @@ const JobSearch = () => {
                 method: "GET",
                 url: `https://jsearch.p.rapidapi.com/search`,
                 headers: {
-                    "X-RapidAPI-Key": '5f169d42demsh08d4280a22a3f92p141e2ejsn6710e2646d5e',
+                    "X-RapidAPI-Key": 'd317731e4bmshe3d859f44a14ffdp13a966jsna8b5f72be968',
                     "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
                 },
                 params: {
